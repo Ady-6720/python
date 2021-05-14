7b78ddf276237ce599d95b20175dc7710f37e54a
Aditya Malode <82941123+Ady-6720@users.noreply.github.com>
2021-05-14 09:03:08 +0530

    Update and rename Ex06_InfoAboutPerson_Dictionaries.py to Info About Person_Dictionaries.py

--- Info About Person_Dictionaries.py
+++ Info About Person_Dictionaries.py
@@ -1,5 +1,7 @@
 #program to giveuser what he wants from predefined dictionary using dictionary operations
 
+print("This program will tell you Name,age,gender,address,phone of a person just enter what you want! ")
+
 info={"name":"John Will","age":"22","gender":"Male","address":"12Street, Bakery road, Berkley","phone":"220-432-8794"}
 i=str(input("What do you want to know: ")).lower() #lower fuction makes user input in lowercase letters completely
 print(info.get(i,"Not found"))
